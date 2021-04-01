b2ab538e28a221d3a2a3ac86cda57bfff175783f
YI <yanaceyizaguirre@gmail.com>
2021-03-31 20:34:35 -0500

    Final Update

--- ForStudents/GnuPlotMaker.java
+++ ForStudents/GnuPlotMaker.java
@@ -9,11 +9,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.*;
 import java.io.*;
-import com.google.gson.*;
+//import com.google.gson.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
-public class GnuPlotMakerTester {
+public class GnuPlotMaker {
 	// REMOVES DOUBLE QUOTE
 		private static String trim(String arg) 
 		{
@@ -34,14 +34,14 @@ public class GnuPlotMakerTester {
 		private static void saveIt(HttpURLConnection conn) throws Exception
 		{
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			System.out.println("buffer reader was created.");
+			//System.out.println("buffer reader was created.");
 			String line;
 		    FileWriter writer = new FileWriter(new File("JSONFile.json"));
-			System.out.println("File Writer was created.");
+			//System.out.println("File Writer was created.");
 			while ((line = in.readLine()) != null)
 		    {
 				writer.write(line);
-		        System.out.println(line);
+		        //System.out.println(line);
 		    }
 			writer.close();
 		    in.close();
@@ -64,12 +64,12 @@ public class GnuPlotMakerTester {
 		{	
 			JSONParser parse = new JSONParser();
 			Object Obj = parse.parse(new FileReader(file));
-			System.out.println("i am here");
+			//System.out.println("i am here");
 			JSONArray toJsonArray = (JSONArray) Obj;//type casting to Json Array
-			System.out.println("i am here 2");
+			//System.out.println("i am here 2");
 				
 			FileWriter	writer = new FileWriter(new File("data.txt"));
-			System.out.println("i am here 3");
+			//System.out.println("i am here 3");
 			for(int i = 0; i<toJsonArray.size(); i++)
 			{
 				JSONObject toJsonObj = (JSONObject) toJsonArray.get(i);
@@ -79,7 +79,7 @@ public class GnuPlotMakerTester {
 				else 
 				{
 				String line = (xVar + " " + yVar+"\n");
-				System.out.println(line);
+				//System.out.println(line);
 				writer.write(line);
 				} 
 			}
@@ -100,11 +100,12 @@ public class GnuPlotMakerTester {
 			writer.close();
 		
 		}
+	//FORKS A NEW PROCESS TO RUN GNU PLOT	
 		private static void forkIt() throws Exception
 		{
 			
 				Runtime runtimeProcess = Runtime.getRuntime();
-				Process process = runtimeProcess.exec("C:/Program Files/gnuplot/bin/gnuplot.exe ./scatter.gnu");
+				Process process = runtimeProcess.exec("/usr/bin/gnuplot ./scatter.gnu");
 				process.waitFor();
 		}
 		
@@ -115,14 +116,14 @@ public class GnuPlotMakerTester {
 				throw new IllegalArgumentException();
 			}
 			String trimmedURL = trim(args[0]);
-		    System.out.println(trimmedURL); //for testing purposes
+		    //System.out.println(trimmedURL); //for testing purposes
 			HttpURLConnection stream = connect(trimmedURL);
-			System.out.println("connection accomplished"); //for testing purposes
+			//System.out.println("connection accomplished"); //for testing purposes
 			saveIt(stream);
-			System.out.println("The json file was saved");
+			//System.out.println("The json file was saved");
 			String input;
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("Please enter a command"); //for testing purposes
+			//System.out.println("Please enter a command"); //for testing purposes
 			while((input = reader.readLine()) != null)
 			{
 				String command = readIt(input,"command");
@@ -136,10 +137,13 @@ public class GnuPlotMakerTester {
 					String x = readIt(input, "xAxis");
 					String y = readIt(input, "yAxis");
 					writeXY("JSONFile.json",x,y);
-					System.out.println("data has been written");
+				//	System.out.println("data has been written");
 					initGnuFile(x,y);
-					forkIt();	 
-					System.out.println("{\"status\":\"success\",\"imageFilename\":\"scatterPlot.png\",\"height\":360,\"width\":480}");
+					forkIt();
+			       		PrintWriter writer = new PrintWriter(System.out); 
+					writer.write("{\"status\":\"success\",\"imageFilename\":\"scatterPlot.png\",\"height\":360,\"width\":480}");
+					writer.flush();		
+					//{System.out.println("{\"status\":\"success\",\"imageFilename\":\"scatterPlot.png\",\"height\":360,\"width\":480}");
 					//{"status":"success","imageFilename":"scatterPlot.png","height":360,"width":480}
 				}
 				else {System.err.println("AHHHH");}
